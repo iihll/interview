0f846bc180dd46ab758ba75a3880fcc34d583389
duanqing.xing <duanqing.xing@brgroup.com>
2022-03-01 10:51:06 +0800

    算法：字符串最长的不重复子串

---
+++ lengthOfLongestSubstring.js
@@ -0,0 +1,31 @@
+// 字符串最长的不重复子串
+/**
+ *
+ * @param {String} s
+ */
+const lengthOfLongestSubstring = function (s) {
+  if (s.length === 0) {
+    return 0
+  }
+
+  let left = 0
+  let right = 1
+  let max = 0
+
+  while (right <= s.length) {
+    let lr = s.slice(left, right)
+    const index = lr.indexOf(s[right])
+
+    if (index > -1) {
+      left = index + left + 1
+    } else {
+      lr = s.slice(left, right + 1)
+      max = Math.max(max, lr.length)
+    }
+    right++
+  }
+
+  return max
+}
+
+console.log(lengthOfLongestSubstring('pwwkew'))
